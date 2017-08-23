dbf845a97ec794b6843c526bba0c4a7d4d0f71d6
sakura-fly <sourcesakura@sina.com>
2017-08-23 14:24:57 +0800

    子弹出界判断

--- src/com/tankwars/dd/Impact.java
+++ src/com/tankwars/dd/Impact.java
@@ -1,12 +1,19 @@
 package com.tankwars.dd;
 
-import com.tankwars.model.MoveObj;
+import com.tankwars.model.Bullet;
+import com.tankwars.model.Tank;
+import com.tankwars.util.Constants;
 
 public class Impact {
-	
-	public boolean out(MoveObj m){
+
+	public boolean out(Tank m) {
 		
 		return false;
+	}
+
+	public boolean out(Bullet m) {
+
+		return m.getX() < 0 || m.getY() < 0 || m.getX() > Constants.WITH_PANEL || m.getY() > Constants.HIGHT_PANEL;
 	}
 
 }
